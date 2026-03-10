7e6655d2bc3e1677d70318b1557adf86bff3dc48
Erick Santos <ericklindo123lindo@gmail.com>
2026-03-10 18:28:09 -0300

    primeiro-commit

---
+++ projeto-aulas/src/Aluno.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Aluno {
+public static void main(String[] args) {
+    int nota;
+
+Scanner scanner = new Scanner(System.in);
+
+System.out.print("Digite a nota do aluno: ");
+nota = scanner.nextInt();
+if (nota >= 6)
+{
+    System.out.println("Aluno aprovado!");
+}
+else
+{
+    System.out.println("Aluno reprovado!");
+
+    
+}
+}
+   
+
+
+
+
+
+
+
+
+}
+
--- projeto-aulas/src/App.java
+++ projeto-aulas/src/App.java
@@ -7,12 +7,14 @@ public static void main(String[] args) {
 
 // Variáveis e constantes
     String nome = "Erick";
+    String sobrenome ="Porto";
     int idade = 10;
     float altura = 1.75f;
     final String NOME_COMPLETO = "Erick Silva";
     idade = 19; 
     String nomeMae = "Deliane";
     long numeroTelefone = 1234567890L;
+    String nomeCompleto = nomeCompleto(nome, sobrenome);
 
     System.out.println("Meu nome é " + NOME_COMPLETO + ", tenho "
      + idade + " anos e minha altura é " + altura + "m. Minha mãe se chama " + nomeMae + 
@@ -34,6 +36,7 @@ public static void main(String[] args) {
 System.out.println("Olá, " + nomeDigitado + "! Bem-vindo ao curso de Java Básico.");
 System.out.println("Seu carro foi fabricado em " + anoFabricacao + ".");
 System.out.println("A soma dos números é: " + (primeiroNumero + segundoNumero));
+System.out.println(nomeCompleto);
 
 idade = 32;
 
@@ -43,4 +46,19 @@ idade = 32;
 
  
 }
+
+
+public static String nomeCompleto(String nome, String sobrenome) {
+    return nome + " " + sobrenome;
+
+
+
+
+
+} 
+
+
+
+  
+
 } 
