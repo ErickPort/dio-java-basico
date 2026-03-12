9e3a0d655d68fc89f0926bdc5b588ae613f06fad
Erick Santos <ericklindo123lindo@gmail.com>
2026-03-12 17:50:47 -0300

    commit-atividades

--- projeto-aulas/src/Soma.java
+++ projeto-aulas/src/Soma.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
 public class Soma {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);   
-
-        System.out.print("Digite a quantidade de termos que deve ter a soma: ");
-        int soma = scanner.nextInt(); 
-        for (int i = 1; i <= soma; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print( );
-            }
-            System.out.println();
-        }   
-
-
-    }
-}
     
+        int numero = 0;
+        int soma = 0;   
 
+        System.out.print("Digite a quantidade de termos 1+11+111... que deseja somar: ");
+        numero = scanner.nextInt(); 
+
+        for (int i = 1; i <= numero; i++) {
+            int termo = 0;
+            for (int j = 1; j <= i; j++) {
+                termo += Math.pow(10, j - 1);
+            }
+            soma += termo;
+        }
+        System.out.println("A soma dos termos é: " + soma);
+
+
+    }}
